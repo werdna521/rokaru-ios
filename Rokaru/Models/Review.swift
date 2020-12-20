76f75800f75aa12b4054da9761712cc97d5c7af8
Stanley <angstanley68@gmail.com>
2020-12-21 00:10:42 +0700

    feat: Add models

---
+++ Rokaru/Models/Review.swift
@@ -0,0 +1,14 @@
+//
+//  Review.swift
+//  Rokaru
+//
+//  Created by Stanley on 21/12/20.
+//
+
+struct Review {
+    var id: Int?
+    var userId: Int?
+    var umkmId: Int?
+    var content: String?
+    var score: Double?
+}
---
+++ Rokaru/Models/UMKM.swift
@@ -0,0 +1,16 @@
+//
+//  UMKM.swift
+//  Rokaru
+//
+//  Created by Stanley on 21/12/20.
+//
+
+struct UMKM {
+    var id: Int?
+    var name: String?
+    var phone: String?
+    var location: String?
+    var lat: Double?
+    var lon: Double?
+    var picture: String?
+}
