e24a471c72d2a3de8ddf3f6c815de4461ba75091
Stanley <angstanley68@gmail.com>
2020-12-21 03:46:27 +0700

    feat: Detail belanja page

--- Rokaru/DetailBelanjaViewController.swift
+++ Rokaru/DetailBelanjaViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import TORoundedButton
 
-class DetailBelanjaViewController: UIViewController {
-
+class DetailBelanjaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var _buttonBayar: RoundedButton!
+    @IBOutlet weak var _tableview: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellItem = tableView.dequeueReusableCell(withIdentifier: "cellBarang") as! BarangTableViewCell
+        
+        cellItem._barangImage.image = UIImage(named: "nasi-goreng")
+        cellItem._barangName.text = "Nasi Goreng"
+        cellItem._barangPrice.text = "Rp25.000 x 1"
+        
+        Materialize.yuan(cellItem._barangImage, chiangkuang: false)
+        
+        return cellItem
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        _tableview.delegate = self
+        _tableview.dataSource = self
+        
+        _buttonBayar.tappedHandler = {
+            self.performSegue(withIdentifier: "gotoBayar", sender: nil)
+        }
     }
     
 
