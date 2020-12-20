2cd29d1a9d56437682c04e2a3a34668af266ed4e
werdna521 <48923485+werdna521@users.noreply.github.com>
2020-12-21 00:11:52 +0700

    feat: create product screen

---
+++ Rokaru/ProductViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ProductViewController.swift
+//  Rokaru
+//
+//  Created by Endru Tjen on 21/12/20.
+//
+
+import UIKit
+import MaterialComponents
+
+class ProductViewController: UIViewController {
+    @IBOutlet weak var _favoriteButton: MDCFloatingButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        _favoriteButton.setBackgroundColor(UIColor(hex: "#F97EBDFF"))
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
