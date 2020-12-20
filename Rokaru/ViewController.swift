75989798ab9f6dec9d669a00cfeb6ebd7b5e4850
werdna521 <48923485+werdna521@users.noreply.github.com>
2020-12-20 19:02:06 +0700

    feat: static runtime inilializations

--- Rokaru/ViewController.swift
+++ Rokaru/ViewController.swift
@@ -7,18 +7,30 @@
 
 import UIKit
 import Koloda
+import MaterialComponents
 
 class ViewController: UIViewController {
     @IBOutlet weak var _kolodaView: KolodaView!
+    @IBOutlet weak var _hateButton: MDCFloatingButton!
+    @IBOutlet weak var _loveButton: MDCFloatingButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         _kolodaView.delegate = self
         _kolodaView.dataSource = self
+        
+        _hateButton.setBackgroundColor(UIColor(hex: "#f1ca20ff"))
+        _loveButton.setBackgroundColor(UIColor(hex: "#f86cb4ff"))
     }
 
-
+    @IBAction func _onLove(_ sender: Any) {
+        _kolodaView.swipe(SwipeResultDirection.right)
+    }
+    
+    @IBAction func _onHate(_ sender: Any) {
+        _kolodaView.swipe(SwipeResultDirection.left)
+    }
 }
 
 extension ViewController: KolodaViewDelegate {
@@ -35,7 +47,7 @@ extension ViewController: KolodaViewDelegate {
 
 extension ViewController: KolodaViewDataSource {
     func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
-        return 5
+        return 20
     }
     
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
@@ -46,5 +58,33 @@ extension ViewController: KolodaViewDataSource {
         view.imageView.layer.cornerRadius = 20
         view.imageView.clipsToBounds = true
         return view
+    }
+}
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
     }
 }
