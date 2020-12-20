30215d6117b0d1eb734a667bb29d8b8a1d0cd780
werdna521 <48923485+werdna521@users.noreply.github.com>
2020-12-20 13:22:11 +0700

    Initial Commit

---
+++ Rokaru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rokaru
+//
+//  Created by Endru Tjen on 20/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
