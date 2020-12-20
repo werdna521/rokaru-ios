0c99b721a8cb8d4f2b81c6da9818f65d805bd847
werdna521 <48923485+werdna521@users.noreply.github.com>
2020-12-20 18:00:48 +0700

    feat: add swiping feature

--- Rokaru/ViewController.swift
+++ Rokaru/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import Koloda
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var _kolodaView: KolodaView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        _kolodaView.delegate = self
+        _kolodaView.dataSource = self
     }
 
 
 }
 
+extension ViewController: KolodaViewDelegate {
+    func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+        koloda.reloadData()
+    }
+    
+    func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
+        let alert = UIAlertController(title: "Congratulation!", message: "Now you're \(index)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
+    }
+}
+
+extension ViewController: KolodaViewDataSource {
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return 5
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        let view = NgeryCard(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width - 48, height: 400))
+        view.titleLabel.text = "Nasi Goreng Pinsen Gilak"
+        view.subtitleLabel.text = "200 m"
+        view.imageView.image = UIImage(named: "nasi-goreng")
+        view.imageView.layer.cornerRadius = 20
+        view.imageView.clipsToBounds = true
+        return view
+    }
+}
